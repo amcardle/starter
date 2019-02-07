523ccec6897da85c7c2b7099e95224033b59a8d1
A McArdle <38906087+amcardle@users.noreply.github.com>
2019-02-07 17:08:39 +0000

    vuex-pathify

--- src/store.js
+++ src/store.js
@@ -1,16 +1,26 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import pathify from 'vuex-pathify'
+import { make } from 'vuex-pathify';
 
 Vue.use(Vuex)
 
+const state = {};
+
 export default new Vuex.Store({
+  getters: {
+    ...make.mutations(state),
+  },
+
   state: {
 
   },
   mutations: {
+    ...make.mutations(state),
 
   },
   actions: {
 
-  }
+  },
+  plugins: [pathify.plugin]
 })
